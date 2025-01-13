8cbb7a36fdfb75bf31c99ef8c6afcb0c227a13c0
vvladutz <vlad.balanescu@s.unibuc.ro>
2025-01-13 04:14:31 +0200

    creat clasa de baza

---
+++ include/Animalut.h
@@ -0,0 +1,20 @@
+//
+// Created by vlad on 1/13/2025.
+//
+
+#ifndef ANIMALUT_H
+#define ANIMALUT_H
+
+class Animalut {
+protected:
+    int varsta;
+    int fericire;
+public:
+    Animalut(int varsta, int fericire);
+    virtual ~Animalut();
+
+    virtual void joaca() = 0;
+    virtual void hraneste() = 0;
+};
+
+#endif //ANIMALUT_H
---
+++ src/Animalut.cpp
@@ -0,0 +1,9 @@
+//
+// Created by vlad on 1/13/2025.
+//
+
+#include "Animalut.h"
+
+Animalut::Animalut(int varsta, int fericire) : varsta{varsta}, fericire{fericire} {}
+
+Animalut::~Animalut() {}
